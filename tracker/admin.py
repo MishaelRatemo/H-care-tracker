93f9eb007160b68b13f5040798856c9b008dd374
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-29 22:18:57 +0300

    Models registrations

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    Hospital,
+    Profile,
+    Store,
+    Item,
+    Donor,
+    
+)
 
 # Register your models here.
+admin.site.register(Hospital)
+admin.site.register(Profile)
+admin.site.register(Store)
+admin.site.register(Item)
+admin.site.register(Donor)
+
