556fbb8aa1325337b56d9917394db0583cfa3127
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-31 04:26:53 +0300

    api updates

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import  path, include
 from . import views
 from rest_framework import routers
+# from .views import UserViewSet
 
 
 router = routers.DefaultRouter()
@@ -8,9 +9,12 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     
-    path('', views.home, name='index'),
+    path('index/', views.home, name='index'),  
+    path('', include(router.urls)),  
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('getusers/', views.get_user),   
+    path('getusers/', views.get_user),  
+    path('items/', views.items), 
     path('save_user/', views.save_user),
+    # path('allusers/',UserViewSet.as_view(), name='users'),
     
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User,Group
 from rest_framework import  viewsets, permissions
+from rest_framework.views import APIView
 from .serializers import (
                  UserSerializer,
                  ProfileSerializers, 
@@ -34,7 +35,10 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes =[permissions.IsAdminUser]
+    # permission_classes =[permissions.IsAuthenticated]
+    
+    
+    
     
 @api_view(['GET'])
 def get_user(request):
@@ -45,17 +49,30 @@ def get_user(request):
     else:
         return Response([])
     
+    
+@api_view(['GET'])
+def items(request):
+    items= Item.objects.all()
+    if items:
+        serialize=UserSerializer(items, many=True)
+        return Response(serialize.data)
+    else:
+        return Response([])
+    
 @api_view(['POST'])
 def save_user(request):
     userdetails = request.data  
     user =userdetails['username']
+    fname = userdetails['first_name']
+    lname = userdetails['last_name']
+    uemail = userdetails['email']    
     passw = userdetails['password'] 
     passw = make_password(passw)
     check_user= User.objects.filter(username=user)   
     if check_user.exists():
         return Response('User already exists, try again')
     else:
-        new_user = User(username=user, password=passw)
+        new_user = User(username=user,first_name=fname,last_name=lname,email=uemail, password=passw)
         new_user.save()
         return Response('User added successfully')
    
