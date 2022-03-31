fd9366ee011a283e070ef2581ad94e85e4defa0a
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-31 04:26:17 +0300

    Models refaactoring

--- tracker/models.py
+++ tracker/models.py
@@ -19,6 +19,13 @@ def create_profile(sender,**kwargs):
 
 post_save.connect(create_profile, sender=User)
 
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 class Item(models.Model):
     image = CloudinaryField('image')    
     name = models.CharField(max_length=200, null=False, blank=False)
@@ -27,28 +34,26 @@ class Item(models.Model):
     reimbursement = models.FloatField(default=0,)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    storages = models.ManyToManyField('Store',  related_name='Storage') 
     
     
-class Store(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    item = models.ManyToManyField(Item,  related_name='Items')
-    
+
 class Donor(models.Model):
     donor_name=models.CharField(max_length=200)
     dispatch=models.PositiveIntegerField(default=0)
-    hospital_name=models.ManyToManyField('Hospital')
     contact = models.CharField(max_length=16)
-    price= models.ForeignKey(Item, on_delete=models.CASCADE)
     
     
 class Hospital(models.Model):
-    hospital_name=models.CharField(max_length=200)
-    donor_name=models.ForeignKey(Donor,on_delete=models.CASCADE, max_length=200)
-    inventory=models.ForeignKey(Store,on_delete=models.CASCADE,)
+    hospital_name=models.CharField(max_length=255)
     address = models.CharField(max_length=100)
      
 
+class Merger(models.Model):
+    user=models.ForeignKey(User,null=True, on_delete=models.CASCADE)
+    inventory=models.ForeignKey(Store,on_delete=models.CASCADE)
+    donor_name=models.ForeignKey(Donor,on_delete=models.CASCADE, max_length=200)
+    hospital_name=models.ForeignKey(Hospital, max_length=255, default='0', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete= models.CASCADE, null=True ,related_name='Items')
+    
+    
    
