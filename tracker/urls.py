e50d39d0135d451b30d4ab7af003705449e06dca
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-04-03 15:53:09 +0300

    few updates

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,8 +9,8 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     
-    path('index/', views.home, name='index'),  
-    path('', include(router.urls)),  
+    path('', views.home, name='index'),  
+    path('api/', include(router.urls)),  
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('getusers/', views.get_user),  
     path('items/', views.items), 
