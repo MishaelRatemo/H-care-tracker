e309202229ef6f49e400608a8cdff4ffa63e0885
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-04-05 18:51:13 +0300

    updated donor requets page

--- tracker/forms.py
+++ tracker/forms.py
@@ -3,7 +3,10 @@ from tracker.models import Registrations
 
 
 class RegistrationForm(forms.ModelForm):
-   
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    confirm_password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    
+    
     class Meta:
         model = Registrations
         fields = '__all__'
