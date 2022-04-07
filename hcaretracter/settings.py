c92e8a4da677441033be0701c71c14daace50eab
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-04-07 22:38:13 +0300

    postgis extension update

--- hcaretracter/settings.py
+++ hcaretracter/settings.py
@@ -105,7 +105,7 @@ ROOT_URLCONF = 'hcaretracter.urls'
 
 DATABASES={
         'default':{
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'ENGINE': 'django.contrib.gis.db.backends.postgis',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
